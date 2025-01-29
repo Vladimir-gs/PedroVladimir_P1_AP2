ac6c57edfd3be1af503ac2bcc1cfeeda13f4331e
PEDRO VLADIMIR GUZMAN SALAZAR <PEDRO_GUZMAN1@ucne.edu.do>
2025-01-29 19:37:07 -0400

    Initial commit

---
+++ app/src/main/java/edu/ucne/pedrovladimir_p1_ap2/MainActivity.kt
@@ -0,0 +1,27 @@
+package edu.ucne.pedrovladimir_p1_ap2
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import edu.ucne.pedrovladimir_p1_ap2.ui.theme.PedroVladimir_P1_AP2Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PedroVladimir_P1_AP2Theme {
+
+
+            }
+        }
+    }
+}
